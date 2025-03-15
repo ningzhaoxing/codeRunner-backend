8316dfb4b941735f3315ed61db0bee9c3a8824ea
kitty <3404593235@qq.com>
2025-03-15 19:47:31 +0800

    修改文件目录

--- internal/domain/client/entity/container.go
+++ internal/domain/client/entity/container.go
@@ -134,7 +134,7 @@ func (client *dockerContainerClient) RunCode(request *proto.ExecuteRequest) (res
 	response.CallBackUrl = request.CallBackUrl
 	// 1. 生成唯一临时目录
 	uniqueID := uuid.New().String()
-	tempDir := filepath.Join("/tmp/tmpDir", uniqueID)
+	tempDir := filepath.Join("/tmp", uniqueID)
 	if err := os.MkdirAll(tempDir, 0755); err != nil {
 		log.Printf("创建临时目录失败: %v", err)
 		return response, fmt.Errorf("docker客户端错误")
